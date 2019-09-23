7c5bf92caff193de384490116d1c3689d776679e
codebender828 <jbakebwa@163.com>
2019-09-24 02:47:17 +0800

    add: attempting to style my codeblocks

--- postcss.config.js
+++ postcss.config.js
@@ -10,13 +10,20 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  whitelist: ['blockquote', 'body', 'html'],
+  whitelistPatterns: [
+    /-(leave|enter|appear)(|-(to|from|active))$/,
+    /^(?!(|.*?:)cursor-move).+-move$/,
+    /^router-link(|-exact)-active$/
+  ]
 })
 
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    require('postcss-nested'),
     ...process.env.NODE_ENV === 'production'
       ? [purgecss]
       : []
